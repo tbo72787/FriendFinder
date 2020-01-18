d300455e5fa30dc32ad5468035cef6976d8eb478
Taylor Zettel <tzettel@gmail.com>
2020-01-18 03:52:45 -0500

    add route to survey.html at '/survey' in htmlroutes.js

--- app/routing/htmlroutes.js
+++ app/routing/htmlroutes.js
@@ -3,9 +3,11 @@ var path = require("path");
 var router = express.Router();
 
 router.get("/", function(req, res) {
-
   res.sendFile(path.join(__dirname, "../public/home.html"));
+});
 
+router.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/survey.html"));
 });
 
 module.exports = router;
