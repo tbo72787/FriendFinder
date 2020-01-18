3ea71cf1fc50fb2e9e2d7b59a9c1eab395dd0f77
Taylor Zettel <tzettel@gmail.com>
2020-01-18 03:48:02 -0500

    created routing for '/' in htmlroutes.js; set route in server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+var htmlR = require("./app/routing/htmlroutes");
+var apiR = require("./app/routing/apiroutes");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(htmlR);
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
